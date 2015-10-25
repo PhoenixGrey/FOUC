17a6006aef78ac81618057e3634e24ea5c6c8a34
phoenix <phoenix>
2015-10-25 09:13:45 +0800

    Fonts Fouc

--- src/JS/fouc.js
+++ src/JS/fouc.js
@@ -1,4 +1,36 @@
+jsFouc = function () {
+  $('div#logo').css('background-color', "000000");
+  $('div#logo').css('background-color', "red");
+
+  $('div#logo').css('background-color', "000000");
+  $('div#logo').css('background-color', "red");
+
+  $('div#logo').css('background-color', "000000");
+  $('div#logo').css('background-color', "red");
+
+  $('div#logo').css('background-color', "000000");
+  $('div#logo').css('background-color', "red");
+
+
+  $('div#logo').css('background-color', "000000");
+  $('div#logo').css('background-color', "red");
+
+  $('div#logo').css('background-color', "000000");
+  $('div#logo').css('background-color', "red");
+
+  $('div#logo').css('background-color', "000000");
+  $('div#logo').css('background-color', "red");
+
+  $('div#logo').css('background-color', "000000");
+  $('div#logo').css('background-color', "red");
+
+
+  $('div#logo').css('background-color', "red");
+  $('div#logo').css('background-color', "003399");
+}
+
+
+
 $(document).ready(function(){
-  console.log("hello");
-  $('body').css('background-color', "000000");
+//  jsFouc();
 });
